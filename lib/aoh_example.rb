c97ef2faa5ca82e5ac36f7768557bec4314c4f07
Jacob Hyde <jacobosity@gmail.com>
2020-03-29 17:04:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,14 +7,37 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+  infinite_jest_characters = [
+     { name:  "Don Gately", occupation: "Live-in Staff" },
+     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+     { name:  "Pat Monteseian", occupation: "Staff" },
+     { name:  "Kate Gompert", occupation: "None" },
+     { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
+  
+  || 
+  
+  infinite_jest_characters = [DON_G, JOELLE_VD, PAT_M, KATE_G. BRUCE_G]
 end
 
+#-----------------------------------------------------------------------------------------
+
 def literal_aoh
+  infinite_jest_characters = [
+     { name:  "Don Gately", occupation: "Live-in Staff" },
+     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+     { name:  "Pat Monteseian", occupation: "Staff" },
+     { name:  "Kate Gompert", occupation: "None" },
+     { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
+   # Should this be sorted? In the spec it says "'the literal_aoh method returns an AoH made made from a literal with sorted elements'" but the results are not sorted. Is this a mistake? 
+  
+  # COURSE NOTES
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
+
+#-----------------------------------------------------------------------------------------
 
 def aoh_lookup(aoh, row, key)
 end
